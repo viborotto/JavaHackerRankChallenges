054b247079d7b3f658cb6e683cf93e44f180c8f2
viborotto <vittoriaborotto@gmail.com>
2020-07-31 20:24:40 -0300

    Module OOP complete

---
+++ src/br/com/vittoria/OOP/MethodOverriding2.java
@@ -0,0 +1,36 @@
+package br.com.vittoria.OOP;
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author Vittoria Borotto
+ *
+ */
+public class MethodOverriding2 {
+
+    class BiCycle{
+        String define_me(){
+            return "a vehicle with pedals.";
+        }
+    }
+
+    class MotorCycle extends BiCycle{
+        String define_me(){
+            return "a cycle with an engine.";
+        }
+
+        MotorCycle(){
+            System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+            String temp=super.define_me(); //Fix this line
+
+            System.out.println("My ancestor is a cycle who is "+ temp );
+        }
+
+    }
+    class Solution{
+        public static void main(String []args){
+            MotorCycle M=new MotorCycle();
+        }
+    }
+}
